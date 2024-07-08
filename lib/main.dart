b72264f7612fd3678dea75c7827f541a72b0ce66
punitsoni22 <punitsoni14476@gmail.com>
2024-07-08 15:45:18 +0530

    splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:site_construct/firebase_options.dart';
 import 'package:site_construct/routes/route.dart';
 import 'package:site_construct/ui/user/splash/splash_screen.dart';
 
-import 'ui/user/navigationMenu/navigation_menu.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -29,7 +27,8 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       getPages: routes,
-      home: NavigationMenu(),
+      home: const SplashScreen(),
+      initialRoute: splashScreen,
     );
   }
 }
